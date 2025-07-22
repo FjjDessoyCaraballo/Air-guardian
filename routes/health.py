0b2866382322221a545f40be458598bad11c3434
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-07-22 17:06:05 +0300

    feature: added all the endpoints. Logic is not present yet.

--- routes/health.py
+++ routes/health.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+import sqlite3
 
 app = FastAPI()
 
-@app.get("/")
+conn = sqlite3.connect("mydb.db")
+
+@app.get("/health")
 def health():
 	return {"status": "OK"}
+
+@app.get("/drones")
+def drones():
+	return
+
+@app.get("/nfz")
+def nfz():
+	return
