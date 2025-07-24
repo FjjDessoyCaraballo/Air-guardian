131770ca61451af4e2902883ea90756c8477bebc
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-07-24 18:14:34 +0300

    feat: fixed merge to work with poetry build

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from routes.drones import drones_route
 from routes.health import health_route
 from routes.nfz import nfz_route
 from dotenv import load_dotenv
-import sqlite3
 
 load_dotenv()
-conn = sqlite3.connect("mydb.db")
 air_guardian = FastAPI()
 air_guardian.include_router(health_route)
 air_guardian.include_router(drones_route)
