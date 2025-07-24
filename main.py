6dba23ebaeb81bd6251d74b578aa60c89295a57d
jackie jan <zakaria.farah55@outlook.com>
2025-07-24 11:36:29 +0300

    exposes dotenv to the environment

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from fastapi import FastAPI
 from routes.drones import drones_route
 from routes.health import health_route
 from routes.nfz import nfz_route
+from dotenv import load_dotenv
 import sqlite3
 
+load_dotenv()
 conn = sqlite3.connect("mydb.db")
 air_guardian = FastAPI()
 air_guardian.include_router(health_route)
