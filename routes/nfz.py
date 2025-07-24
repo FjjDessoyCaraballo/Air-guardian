8c6b0a6521804d85b949ee3962323f3d2b8fc314
jackie jan <zakaria.farah55@outlook.com>
2025-07-24 11:26:37 +0300

    endpoint is protected against unauthorized users

--- routes/nfz.py
+++ routes/nfz.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, HTTPException
+from utils.nfz import authenticate
 
 nfz_route = APIRouter()
 
 @nfz_route.get("/nfz")
-def nfz():
+def nfz(req: Request):
+	authenticated = authenticate(req.headers.get("X-Secret"))
+	if not authenticated:
+		return HTTPException(401, "Unauthorized")
+	# fetch from db
 	return
