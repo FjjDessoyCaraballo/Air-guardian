6557a7f9edfcbb27710b71e9b8a0d3225e172828
jackie jan <zakaria.farah55@outlook.com>
2025-07-24 11:34:16 +0300

    added func that appends owner details to the drones that violate nfz

--- utils/drones.py
+++ utils/drones.py
@@ -1,4 +1,6 @@
 import math
+import requests
+import os
 
 def violated_drones(drones):
     violated = []
@@ -7,3 +9,19 @@ def violated_drones(drones):
         if distance < 1000:
             violated.append(drone)
     return violated
+
+def append_owner_details(violated_drones):
+    for drone in violated_drones:
+        url = os.environ["DRONES_API_BASE_URL"] + "users/" + str(drone["owner_id"])
+        req = requests.get(url)
+        if req.status_code != 200:
+            return False
+        try:
+            body = req.json()
+            drone["first_name"] = body["first_name"]
+            drone["last_name"] = body["last_name"]
+            drone["social_security_number"] = body["social_security_number"]
+            drone["phone_number"] = body["phone_number"]
+        except:
+            return False
+    return True
