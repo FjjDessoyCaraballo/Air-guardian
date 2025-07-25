dd594872b29153805eb00728d622b5812424e667
FjjDessoyCaraballo <fdessoycaraballo@gmail.com>
2025-07-25 12:58:02 +0300

    chore: saving progress [db not created and refuses to connect]

--- utils/database.py
+++ utils/database.py
@@ -1,13 +1,28 @@
 import psycopg
 from dotenv import load_dotenv
 import os
+import time
+import subprocess
 
 DB_PASSWORD = load_dotenv()
 print(f'password for the db: {os.getenv('DB_PASSWORD')}')
 
 
 def create_db():
-	with psycopg.connect(f"dbname=nfz_list user=fdessoy- password=super_secret_password") as conn:
+
+	print(50*'*')
+	print("Creating postgreSQL database...")
+	print(50*'*')
+	subprocess.run(
+            f"docker run -d --name airguardian-db -e POSTGRES_DB={os.getenv('DB_NAME')} -e POSTGRES_USER={os.getenv('DB_USER')} -e POSTGRES_PASSWORD={os.getenv('DB_PASSWORD')} -p 5436:5436 postgres:15 || docker start airguardian-db", 
+            shell=True, 
+            check=False,
+            capture_output=True
+        )
+	print(50*'*')
+	time.sleep(5)
+
+	with psycopg.connect(f"host=localhost port=5436 dbname=nfz_list user=fdessoy- password={os.getenv('DB_PASSWORD')}") as conn:
 		with conn.cursor() as cur:
 			cur.execute("""
 			   CREATE TABLE nfz_offender (
@@ -23,5 +38,4 @@ def create_db():
 				)
 				""")
 			conn.commit()
-			conn.close()
 	return print("end of create_db")
